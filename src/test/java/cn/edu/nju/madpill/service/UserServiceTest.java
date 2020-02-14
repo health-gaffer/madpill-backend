ddcfc94caf3346ab15fef1d621215df2d51f2b29
Hiki <aneureka2@gmail.com>
2020-02-14 22:54:28 +0800

    Add user service test

--- src/test/java/cn/edu/nju/madpill/service/UserServiceTest.java
+++ src/test/java/cn/edu/nju/madpill/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.test.autoconfigure.AutoConfigureMybatis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @Log
 @SpringBootTest
+@AutoConfigureMybatis
 @MapperScan("cn.edu.nju.madpill.*mapper")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserServiceTest {
