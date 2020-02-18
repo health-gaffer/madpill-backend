7fd9fdda611de346164b958aef0ead62edc9ed84
aneureka <aneureka2@gmail.com>
2020-02-18 11:12:16 +0800

    Add getUserByToken

--- src/main/java/cn/edu/nju/madpill/exception/ExceptionSuppliers.java
+++ src/main/java/cn/edu/nju/madpill/exception/ExceptionSuppliers.java
@@ -1,5 +1,7 @@
 package cn.edu.nju.madpill.exception;
 
+import org.springframework.http.HttpStatus;
+
 import java.util.function.Supplier;
 
 /**
@@ -11,4 +13,5 @@ import java.util.function.Supplier;
  */
 public class ExceptionSuppliers {
     public static final Supplier<BaseException> DRUG_NOT_FOUND = () -> new BaseException("找不到相应的药品", 404);
+    public static final Supplier<BaseException> INVALID_TOKEN = () -> new BaseException("token 无效", HttpStatus.UNAUTHORIZED.value());
 }
