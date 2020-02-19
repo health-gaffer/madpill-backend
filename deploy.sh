f2294758facfb2e5038429121ecd6589f8e84b1b
CharlesFeng <fdfengjunjie@126.com>
2020-02-19 19:52:01 +0800

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ else
     kill -9 "$TEMP_PID"
 fi
 
-nohup java -jar madpill-backend-0.0.1-SNAPSHOT.jar -Djasypt.encryptor.password="$JASYPT_ENCRYPTOR_PASSWORD" < /dev/null > /dev/null 2>&1 &
+nohup java -jar madpill-backend-0.0.1-SNAPSHOT.jar -Djasypt.encryptor.password="$JASYPT_ENCRYPTOR_PASSWORD" -Dspring.profiles.active=prod < /dev/null > /dev/null 2>&1 &
 echo "[ madpill backend started ]"
 
 exit 0
