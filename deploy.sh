1529d0288a80b691c46f5ab785f7d1c06372e6fb
Shenmiu <gcm971016@gmail.com>
2020-02-29 16:37:24 +0800

    check: check if env is in the session

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ else
     echo "[ previous madpill-backend process pid is:$TEMP_PID, going to be killed ]"
     kill -9 "$TEMP_PID"
 fi
-
+echo "[nohup java -Dspring.profiles.active=prod -Djasypt.encryptor.password=${JASYPT_ENCRYPTOR_PASSWORD} -jar madpill-backend-0.0.1-SNAPSHOT.jar < /dev/null > /dev/null 2>&1 &]"
 nohup java -Dspring.profiles.active=prod -Djasypt.encryptor.password=${JASYPT_ENCRYPTOR_PASSWORD} -jar madpill-backend-0.0.1-SNAPSHOT.jar < /dev/null > /dev/null 2>&1 &
 echo "[ madpill backend started ]"
 
